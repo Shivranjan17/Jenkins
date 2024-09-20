0b41e2ef4842bd6e295b07519decd54a1f8d26d4
Shivranjan Pathak <shivranjanpathak792@gmail.com>
2024-09-20 18:19:24 +0530

    Update jenkins.c

--- jenkins.c
+++ jenkins.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
 int main(){
-    printf("Jay Ganesh Jay Gajanan Har Har Mahadev ...");
+    printf("Jay Ganesh Jay Gajanan Har Har Mahadev Om namoh parvati pateha har har mahadev ...");
     return 0;
 }
